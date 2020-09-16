fc3a5185bd50174d483c86a745ba4b97746a97b9
Eric Zhao <21zhaoe@protonmail.com>
2020-09-15 21:42:04 -0400

    fix: Tera template rendering logic

--- src/templating/tera.rs
+++ src/templating/tera.rs
@@ -2,11 +2,8 @@ use anyhow::Result;
 use serde::Serialize;
 use tera::{Context, Tera};
 
-use std::path::Path;
-
-pub fn render<P: AsRef<Path>, S: Serialize>(path: P, context: &S) -> Result<String> {
-    let path_str = &path.as_ref().to_string_lossy();
-    let templater = Tera::new(path_str)?;
-    let rendered = templater.render(path_str, &Context::from_serialize(&context)?)?;
+pub fn render<S: Serialize>(template: &str, context: &S) -> Result<String> {
+    let context = Context::from_serialize(&context)?;
+    let rendered = Tera::one_off(template, &context, false)?;
     Ok(rendered)
 }
