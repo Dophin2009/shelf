6c6f867d790897f396bfeb1f34f75e51b00bd863
Eric Zhao <21zhaoe@protonmail.com>
2021-07-31 17:46:38 -0400

    Fresh start

--- src/template/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod gotmpl;
-pub mod hbs;
-pub mod tera;
--- src/template/tera.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-use serde::Serialize;
-use tera::{Context, Tera};
-
-pub fn render<S: Serialize>(template: &str, context: &S) -> Result<String> {
-    let context = Context::from_serialize(&context)?;
-    let rendered = Tera::one_off(template, &context, false)?;
-    Ok(rendered)
-}
