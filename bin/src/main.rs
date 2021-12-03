3d33dcaea9be8cbd3709237be5a735abbb8df6d8
Eric Zhao <21zhaoe@protonmail.com>
2021-12-03 17:52:09 -0500

    refactor(bin): Move cli fns to main

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,15 +1,13 @@
 #[macro_use]
 mod output;
 
-mod cli;
 mod load;
 
+use std::env;
 use std::process;
 
 use bunt_logger::{ColorChoice, Level};
 use clap::Parser;
-
-use crate::cli::Options;
 
 fn main() {
     let opts = Options::parse();
@@ -23,7 +21,37 @@ fn main() {
         })
         .stderr(ColorChoice::Auto);
 
-    if cli::cli(opts).is_err() {
+    if cli(opts).is_err() {
         process::exit(1);
     }
+}
+
+#[derive(Parser, Debug)]
+#[clap(version = clap::crate_version!(), author = clap::crate_authors!(), about = clap::crate_description!())]
+pub struct Options {
+    #[clap(short, long, parse(from_occurrences), about = "Message verbosity")]
+    pub verbosity: usize,
+    #[clap(short, long, about = "Silence all output")]
+    pub quiet: bool,
+    #[clap(short, long, about = "Pretend to process")]
+    pub noop: bool,
+
+    #[clap(short, long, about = "Linking destination (defaults to $HOME)")]
+    pub home: Option<String>,
+
+    #[clap(required = true)]
+    pub packages: Vec<String>,
+}
+
+#[inline]
+pub fn cli(opts: Options) -> Result<(), ()> {
+    run(opts).map_err(|_| tl_error!("{$red+bold}Fatal errors were encountered! See above.{/$}"))
+}
+
+#[inline]
+fn run(opts: Options) -> Result<(), ()> {
+    tl_info!("Loading packages");
+    let _graph = crate::load::load(&opts.packages);
+
+    Ok(())
 }
