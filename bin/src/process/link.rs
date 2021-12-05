d19fd8f27d234637f42295147ece3de52710695d
Eric Zhao <21zhaoe@protonmail.com>
2021-12-05 14:29:25 -0500

    feat(bin): More diagnostic messages for LinkAction

--- bin/src/process/link.rs
+++ bin/src/process/link.rs
@@ -1,14 +1,15 @@
 use std::path::PathBuf;
 
-use shelflib::action::link::{Res, Skip};
-use shelflib::action::{link::Error, LinkAction, Resolve};
+use shelflib::action::{
+    link::{Error, Op, Res, Skip},
+    LinkAction, Resolve,
+};
 
 use crate::ctxpath::CtxPath;
-use crate::pretty::error;
 use crate::pretty::{
-    arrshowdim,
-    output::{sl_debug, sli_error, sli_warn, slii_error, slii_warn},
-    ppath, skipping, var,
+    arrshowdim, error,
+    output::{sl_debug, sl_info, sli_debug, sli_error, sli_warn, slii_error, slii_warn},
+    ppath, skipping, var, warning,
 };
 
 #[inline]
@@ -16,12 +17,14 @@ pub fn process(action: LinkAction, path: &CtxPath, dest: &PathBuf) -> Result<(),
     let src = CtxPath::new(&action.src, path.abs()).unwrap();
     let dest = CtxPath::new(&action.dest, dest).unwrap();
 
-    sl_debug(format!(
+    sl_info(format!(
         "{} {} to {}",
         if action.copy { "Copying" } else { "Linking" },
         ppath(src.rel()),
         ppath(dest.rel())
     ));
+    sli_debug(arrshowdim(src.abs().display()));
+    sli_debug(arrshowdim(dest.abs().display()));
 
     let res = match action.resolve() {
         Ok(res) => res,
@@ -32,12 +35,28 @@ pub fn process(action: LinkAction, path: &CtxPath, dest: &PathBuf) -> Result<(),
     };
 
     match res {
-        Res::Normal(ops) => todo!(),
-        Res::Overwrite(ops) => todo!(),
+        Res::Normal(ops) => process_normal(&src, &dest, ops),
+        Res::Overwrite(ops) => process_overwrite(&src, &dest, ops),
         Res::Skip(skip) => process_skip(&src, &dest, skip),
     }
 
     Ok(())
+}
+
+#[inline]
+fn process_normal(src: &CtxPath, dest: &CtxPath, ops: Vec<Op>) {
+    // TODO: Implement
+}
+
+#[inline]
+fn process_overwrite(src: &CtxPath, dest: &CtxPath, ops: Vec<Op>) {
+    sli_warn(warning(format!(
+        "{} will be overwritten",
+        ppath(dest.rel())
+    )));
+    slii_warn(arrshowdim(dest.abs().display()));
+
+    // TODO: Implement
 }
 
 #[inline]
@@ -52,8 +71,22 @@ fn process_skip(src: &CtxPath, dest: &CtxPath, skip: Skip) {
             slii_warn(arrshowdim(src.abs().display()));
             slii_warn(arrshowdim(dest.abs().display()));
         }
-        Skip::OptMissing => {}
-        Skip::DestExists => {}
+        Skip::OptMissing => {
+            sli_warn(skipping(format!(
+                "optional {} {} is missing",
+                var("src"),
+                ppath(src.rel())
+            )));
+            slii_warn(arrshowdim(src.abs().display()));
+        }
+        Skip::DestExists => {
+            sli_warn(skipping(format!(
+                "{} {} already exists",
+                var("dest"),
+                ppath(dest.rel())
+            )));
+            slii_warn(arrshowdim(dest.abs().display()));
+        }
     }
 }
 
