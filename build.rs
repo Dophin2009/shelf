703a267554034a32f0859848e1a40ac42a4bcdf3
Eric Zhao <21zhaoe@protonmail.com>
2022-05-04 17:09:58 -0400

    build: Add lua-vendor feature

--- build.rs
+++ build.rs
@@ -4,7 +4,8 @@ fn main() {
         feature = "lua53",
         feature = "lua52",
         feature = "lua51",
-        feature = "luajit"
+        feature = "luajit",
+        feature = "lua-vendor"
     )))]
     compile_error!("You must enable one of the features: lua54, lua53, lua52, lua51, luajit");
 }
